aa0f0e51bb03ab345fe7976b3b7ea6aab3a8ebc4
Raúl Bocanegra <raul.bocanegra@shapelets.io>
2019-10-05 01:15:11 +0200

    Update CI files Filter unsupported builds on Windows

--- build.py
+++ build.py
@@ -3,8 +3,15 @@
 
 from bincrafters import build_template_default
 
+
+def _is_shared_msvc_build(build):
+    return not(build.options['benchmark:shared'] == True and build.settings['compiler'] == 'Visual Studio')
+
+
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
-    
-    builder.run()
+    filter(_is_shared_msvc_build, builder.items)
+
+    print(builder.items)
+    #builder.run()
