a9be96c9f4991c105b12d5d76e6cd89bc53f5553
Raul Bocanegra Algarra <raul.bocanegra.algarra@gmail.com>
2019-10-06 07:40:54 +0200

    Stable/1.4.1 (#14)
    * Added pending platforms
    
    * Suggestions from inclusion requirements applied
    
    Signed-off-by: raulbocanegra <raul.bocanegra.algarra@gmail.com>
    
    * Filter unsupported build on Windows
    
    * remove unnecesary line, builds are filtered on build.py
    
    * Added pending platforms
    
    * Suggestions from inclusion requirements applied
    
    Signed-off-by: raulbocanegra <raul.bocanegra.algarra@gmail.com>
    
    * Filter unsupported build on Windows
    
    * remove unnecesary line, builds are filtered on build.py
    
    * Avoid filtering builds
    
    * Fix options for testing related cmake opttions
    
    * Avoid filtering builds
    
    * Fix options for testing related cmake opttions
    
    * Fix filter builds for Visual Studio
    
    * Swith on build
    
    * Remove pending testing options
    
    * move removal of options.shared to configure method
    
    * Update conanfile and add conandata
    
    * Update CI files
    Filter unsupported builds on Windows
    
    * Fix build.py
    
    * Remove testing options.
    
    * Remove unused error
    Add env vars to ci

--- build.py
+++ build.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 from bincrafters import build_template_default
-import platform
+
+
+def _is_shared_msvc_build(build):
+    return not(build.options['benchmark:shared'] == True and build.settings['compiler'] == 'Visual Studio')
+
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
-    not_suported_builds = []
-    os_name = platform.system()
-    for build in builder.items:
-        settings, options, env_vars, build_requires, reference = build
-        if (settings["compiler"] == "Visual Studio" and settings["arch"] == "x86") or (os_name == "Windows" and options["benchmark:shared"]):
-            not_suported_builds.append(build)
-        
-    for not_supported in not_suported_builds:
-        builder.items.remove(not_supported)
+    builder.builds = filter(_is_shared_msvc_build, builder.items)
     builder.run()
